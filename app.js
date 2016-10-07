033ddb34a627bef7cb26485e7fb365d28b154671
ctsague <tsagueymele@yahoo.fr>
2016-10-07 14:18:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
 // good luck !
 
-var inputs = $(".color");
-console.log(inputs);
-//input.color input.color...
-var len = inputs.length;
-console.log(len);
-//10
-for (var i=0; i<len; i++){
-//var colors = inputs[i];
-var color = $(inputs[i]).data('color');
-//console.log(colors);
-console.log(color);
-$(inputs[i]).css("background-color", color);
+function recupColors(){
+	var inputs = $(".color");
+	var len = inputs.length;
+	for (var i=0; i<len; i++){
+		var color = $(inputs[i]).data('color');
+		console.log(color);
+		$(inputs[i]).css("background-color", color);
+	}
 }
+recupColors();
 
 $('input.color').on('click', function(){
-	var font = $(this).data('color');
-	
+	var fond = $(this).data('color');
 	var appel = checked();
-	console.log(appel);
 	if(appel){
-		$(".main").css('color',font)
+		$(".main").css('color',fond)
 	} else {
-	$('body').css("background-color", font);
+	$('body').css("background-color", fond);
 		
-	}
-	
+	}	
 });
-
-
 
 function checked(){
 return $('#modify-texte').is(':checked');
-
 }
